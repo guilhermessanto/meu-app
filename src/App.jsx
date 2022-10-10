153509711644885d24cddb58b7a00f4242829c3a
Guilherme Santana <g.santossantana22@gmail.com>
2022-10-10 11:25:29 -0300

    componentes
    combinar as tags com fragmentação

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-function App() {
-  return( 
-    <div>
-      <h1>Olá mundo!</h1>
-      <h2>Compreendendo react</h2>
-    </div>
-  )
-  
-}
+import Cabecalho from "./components/layouts/Cabecalho";
+import Conteudo from "./components/layouts/Conteudo";
+import Footer from "./components/layouts/Footer";
+const App = () => {
+  return (
+    <>
+      {/* Fragmento */}
+      <Cabecalho />
+      <Conteudo />
+      <Footer />
+      {/* Fim fragmento */}
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/layouts/Cabecalho.jsx
@@ -0,0 +1,10 @@
+import Menu from "../Menu";
+const Cabecalho = () => {
+  return (
+    <header>
+      <h1>Interface react</h1>
+      <Menu />
+    </header>
+  );
+};
+export default Cabecalho;
