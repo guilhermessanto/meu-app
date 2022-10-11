ef0455bcf5ef9e024144b6e1bf2688caa1c07328
Guilherme Santana <g.santossantana22@gmail.com>
2022-10-11 11:27:24 -0300

    styled-components

--- src/components/layouts/Cabecalho.jsx
+++ src/components/layouts/Cabecalho.jsx
@@ -1,10 +1,31 @@
+import styled from "styled-components";
 import Menu from "../Menu";
+const periodo = "diurno";
+const StyledCabecalho = styled.header`
+  background-color: ${periodo === "diurno" ? "lightblue" : "darkblue"};
+  text-align: center;
+
+  h1 {
+    color: black;
+  }
+  p {
+    font-weight: bold;
+    color: blue;
+    font-size: 2rem;
+    &:hover {
+      color: red;
+      cursor: pointer;
+    }
+  }
+`;
+
 const Cabecalho = () => {
   return (
-    <header>
+    <StyledCabecalho>
       <h1>Interface react</h1>
       <Menu />
-    </header>
+      <p>teste</p>
+    </StyledCabecalho>
   );
 };
 export default Cabecalho;
