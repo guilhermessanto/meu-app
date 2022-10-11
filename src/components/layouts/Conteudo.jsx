6ff13ba3af4d032f84886863f70598e510ecab02
Guilherme Santana <g.santossantana22@gmail.com>
2022-10-11 08:51:51 -0300

    Conflitos css

--- src/components/layouts/Conteudo.jsx
+++ src/components/layouts/Conteudo.jsx
@@ -1,9 +1,10 @@
 import Artigo from "../Artigo";
+import "./Conteudo.css";
 const Conteudo = () => {
   return (
     <main>
       <section>
-        <h2>Conteúdo do site</h2>
+        <h2 className="subtitulo">Conteúdo do site</h2>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati,
           aliquid fugit perferendis, molestiae in architecto, suscipit magnam
