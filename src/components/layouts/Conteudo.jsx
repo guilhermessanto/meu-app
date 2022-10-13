b92785c7303ca9890917281713da10804487319f
Guilherme Santana <g.santossantana22@gmail.com>
2022-10-13 11:33:58 -0300

    mudando cor de fundo

--- src/components/layouts/Conteudo.jsx
+++ src/components/layouts/Conteudo.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import Artigo from "../Artigo";
 const Conteudo = () => {
+  const [corFundo, setCorFundo] = useState();
+  const [contador, setContador] = useState(0);
+
+  const fundoAmarelo = () => setCorFundo("#FFEE71");
+  const fundoAzul = () => setCorFundo("lightBlue");
+  const fundoVermelho = () => setCorFundo("lightpink");
+
+  const atualizaContagem = () => setContador(contador + 1);
+
   return (
-    <main>
+    <main style={{ backgroundColor: corFundo }}>
+      <button onMouseOver={fundoAmarelo}>Amarelo</button>
+      <button onMouseOver={fundoAzul}>Azul</button>
+      <button onMouseOver={fundoVermelho}>Vermelho</button>
       <section>
-        <h2>Conteúdo do site</h2>
+        <h2 onClick={atualizaContagem}>Contador: {contador}</h2>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati,
           aliquid fugit perferendis, molestiae in architecto, suscipit magnam
