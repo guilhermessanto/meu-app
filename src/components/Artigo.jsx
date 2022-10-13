547bfc289aa4a8a786d3617bb7d06222d098f3d9
Guilherme Santana <g.santossantana22@gmail.com>
2022-10-13 09:29:25 -0300

    versão usando destructuring

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -1,10 +1,13 @@
-const Artigo = (props) => {
+/* Acesso às props usando destructuring de objeto */
+/* const Artigo = (props)=>{}  */
+const Artigo = ({ titulo, subtitulo, curso, children }) => {
   return (
     <article>
-      <h1>{props.titulo}</h1>
-      <h2>{props.subtitulo}</h2>
-      <h4>{props.curso}</h4>
-      <p>{props.children}</p>
+      {/* <h1>{props.titulo}</h1> */}
+      <h1>{titulo}</h1>
+      <h2>{subtitulo}</h2>
+      <h4>{curso}</h4>
+      <p>{children}</p>
     </article>
   );
 };
