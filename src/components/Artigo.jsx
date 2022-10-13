d000b8c4d5aed71284ca1b8e5c925e281d28c44b
Guilherme Santana <g.santossantana22@gmail.com>
2022-10-13 09:18:57 -0300

    props exemplo 01

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -1,13 +1,10 @@
-const Artigo = () => {
+const Artigo = (props) => {
   return (
     <article>
-      <h3>Titulo...</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse veritatis
-        blanditiis sed vero illo excepturi qui reiciendis, deserunt, itaque,
-        mollitia facere dolore vitae harum veniam iste. Voluptas, adipisci modi?
-        Maiores.
-      </p>
+      <h1>{props.titulo}</h1>
+      <h2>{props.subtitulo}</h2>
+      <h4>{props.curso}</h4>
+      <p>{props.children}</p>
     </article>
   );
 };
--- src/components/layouts/Conteudo.jsx
+++ src/components/layouts/Conteudo.jsx
@@ -1,5 +1,12 @@
 import Artigo from "../Artigo";
 const Conteudo = () => {
+  /* Cire um array com o nome de tres cursos */
+  const cursos = [
+    "Front-end saia do zero",
+    "Back-end como você nunca viu",
+    "testes unitarios",
+  ];
+  /* Dentro das chamadas do artigo, passa como uma prop o nome de cada curso */
   return (
     <main>
       <section>
@@ -10,9 +17,15 @@ const Conteudo = () => {
           debitis nam repudiandae hic! Porro maiores cupiditate, autem doloribus
           provident tempore tempora natus.
         </p>
-        <Artigo />
-        <Artigo />
-        <Artigo />
+        <Artigo curso={cursos[0]} titulo="Artigo 1" subtitulo="subtitulo 1">
+          Este é o texto do primeiro artigo...
+        </Artigo>
+        <Artigo curso={cursos[1]} titulo="Artigo 2" subtitulo="subtitulo 2">
+          Este é o texto do segundo artigo...
+        </Artigo>
+        <Artigo curso={cursos[2]} titulo="Artigo 3" subtitulo="subtitulo 2">
+          Este é o texto do terceiro artigo...
+        </Artigo>
       </section>
     </main>
   );
