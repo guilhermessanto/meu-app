efdf1bef7873e6f5045a0910e6acd41801e073ba
Guilherme Santana <g.santossantana22@gmail.com>
2022-10-13 10:40:47 -0300

    evento onClick

--- src/components/layouts/Cabecalho.jsx
+++ src/components/layouts/Cabecalho.jsx
@@ -1,7 +1,13 @@
 import Menu from "../Menu";
 const Cabecalho = () => {
+  const exemplo2 = () => {
+    console.log("Exemplo2");
+  };
   return (
     <header>
+      <button onClick={() => console.log("exemplo1")}>Exemplo 1 </button>
+      <button onClick={exemplo2}>Exemplo 2 </button>
+      <hr />
       <h1>Interface react</h1>
       <Menu />
     </header>
